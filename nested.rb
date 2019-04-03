a48912871eb5cbe0198bf6ae9285ee5701e2120f
Joel S <joel29dec@gmail.com>
2019-04-03 18:14:21 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,17 +56,20 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     str = programmer_hash[:dennis_ritchie][:languages]
+     str[0]
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
+ 
 	programmer_hash = 
  		{
+ 		    :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        },
+
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -91,7 +95,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
+          :known_for => "GUI",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
@@ -99,9 +103,9 @@ def changing_alan
           :languages => ["C"]
         }
      }
+   
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
+     #alans_new_info = "GUI"
      
 end
 
@@ -118,8 +122,12 @@ def adding_to_dennis
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => ["C", "Assembly"]
         }
      }
-
+ 
 end
+
+#programmer_hash = {:grace_hopper => {:known_for => "COBOL", :languages => ["COBOL", "FORTRAN"]},
+       #:alan_kay => {:known_for => "Object Orientation", :languages => ["Smalltalk", "LISP"]},
+       # :dennis_ritchie => {:known_for => "Unix", :languages => ["C"]}}
